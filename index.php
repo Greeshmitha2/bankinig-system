6fd1d809cd45e51b6d373d0cd275eb9de79f4e42
Greeshmitha2 <95158695+Greeshmitha2@users.noreply.github.com>
2021-12-15 08:02:53 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <title>SPARKS Bank</title>
+   <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<!-- Navigation bar-->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">SPARKS Bank</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="customers.php">Our Customers</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="transactions.php">Transfer History</a>
+        </li>
+         <li class="nav-item">
+        
+        </li>
+        
+</div>
+      </ul>
+      
+    </div>
+  </div>
+</nav>
+<!-- Caraousel-->
+<div id="demo" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  
+  <!-- The slideshow -->
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://www.marshallstrategy.com/wp-content/uploads/2016/10/bank1.jpg" alt="Easy Transfer" width="1100" height="500">
+      <div class="carousel-caption">
+    <h3>Easy Money Transfer</h3>
+    </div>
+    </div>
+    <div class="carousel-item">
+      <img src="https://akm-img-a-in.tosshub.com/businesstoday/images/story/202003/bank_2_660x450_240120042324_300320012953.jpg" alt="Quick Deposit" width="1100" height="500">
+      <div class="carousel-caption  d-none d-md-block">
+    <h3>Quick Personal Loans</h3>
+    </div>
+    </div>
+    <div class="carousel-item">
+      <img src="https://thumbs.dreamstime.com/b/bank-counter-currency-exchange-service-managers-clients-atm-customer-cartoon-male-female-characters-flat-vector-193263420.jpg" width="1100" height="500">
+      <div class="carousel-caption">
+    <h3>Customer Satisfaction</h3>
+    </div>
+    </div>
+  </div>
+  
+  
+  <!-- Left and right controls -->
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+<!--About us-->
+<section class= "my-5">
+<div class="py-5">
+ <h2 class="text-center" >About Us</h2>
+ </div>
+<div class="container-fluid">
+ <div class ="row">
+<div  class="col-lg-6 col-md-6 col-12">
+      <img src="https://i.pinimg.com/originals/1f/8f/e5/1f8fe515009108b01c3484e520a0cb81.jpg" class="img-fluid">
+      </div>
+      <div  class="col-lg-6 col-md-6 col-12">
+      <h4>SPARKS Bank</h4>
+      <p1>SPARKS Bank is an Indian financial services company with its registered office in Hyderabad, Telangana, and corporate office in Bengaluru, Karnataka.
+               It offers a wide range of banking products and financial services for corporate and retail customers through a variety of delivery 
+               channels and specialized subsidiaries in the areas of investment banking, life, non-life insurance, venture capital and asset management. The bank has a network of 5,275 branches and 15,589 ATMs across India and has a presence in 17 countries.
+        </p1>
+        </div>  
+         
+ </div>
+ </div>
+<!--Dsiclaimer-->
+</section>
+<section class= "my-5">
+<div class="py-5">
+ <h3 class="text-center" >Disclaimer</h3>
+ <p1>This website is the
+      outcome of a intern project, and does not
+      necessarily represent the views of any organisation or any other individuals referenced or
+      acknowledged within the website. Anyone may reproduce, distribute, adapt, translate the content on the website, without explicit permission, provided that the content is accompanied by an acknowledgement that SPARKS Bank website is the source and that it is clearly indicated if changes were made to the original content.</p1>
+ </div>
+<footer class="text-center mt-2 py-5">
+        <p>&copy 2021  <b> Greeshmitha </b> </br>SPARKS Bank</p>
+      </footer>
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>  
+</body>
+</html> 
